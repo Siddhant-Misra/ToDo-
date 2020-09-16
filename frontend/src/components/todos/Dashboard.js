082177470ecd00098f8f91e712b1ca123e99eca4
Siddhant Misra <msiddhant22@gmail.com>
2020-09-15 22:31:07 -0700

    first commit

---
+++ frontend/src/components/todos/Dashboard.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react';
+import TodoCreate from './TodoCreate'; 
+import TodoList from './TodoList';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className='ui container'>
+        <TodoCreate /> 
+        <TodoList />
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
